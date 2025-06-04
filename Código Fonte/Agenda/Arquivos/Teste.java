2cb1bda6979d969871970ffb4a1c9cb30059c479
Miguel-MC-png <miguel.coisaseria@gmail.com>
2025-06-03 21:36:56 -0300

    Add files via upload

---
+++ Código Fonte/Agenda/Arquivos/Teste.java
@@ -0,0 +1,16 @@
+public class Teste
+{
+    public static void main(String args[])
+    {
+        Agenda agenda = new Agenda();
+        MaquinaLavar teste = new MaquinaPequena();
+        
+        agenda.marcarHorario(9, 10, 11, 123, 4);
+        agenda.marcarHorario(0, 10, 11, 123, 4);
+        agenda.marcarHorario(1, 10, 11, 123, 4);
+        
+        agenda.marcarHorario(1, 10, 11, 789, 4);
+        agenda.marcarHorario(2, 10, 11, 789, 4);
+        agenda.marcarHorario(3, 10, 11, 789, 4);
+    }
+}
