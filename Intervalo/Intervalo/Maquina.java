eb95f36e599b9ce5930c1c8d28203e9198185c6a
Miguel-MC-png <miguel.coisaseria@gmail.com>
2025-06-26 01:08:21 -0300

    Add files via upload

---
+++ Intervalo/Intervalo/Maquina.java
@@ -0,0 +1,15 @@
+
+public class Maquina
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private int id;
+
+    /**
+     * Construtor para objetos da classe Maquina
+     */
+    public Maquina(int id)
+    {
+        // inicializa variáveis de instância
+        this.id = id;
+    }
+}
