dd7520f7ce5a0e6a2446d72e374292041234068b
Miguel-MC-png <miguel.coisaseria@gmail.com>
2025-06-25 22:53:50 -0300

    Add files via upload

---
+++ Intervalo/Reserva.java
@@ -0,0 +1,16 @@
+
+public class Reserva
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private User user;
+    private IntervaloDeUso intervalo;
+
+    /**
+     * Construtor para objetos da classe Reserva
+     */
+    public Reserva()
+    {
+        // inicializa variáveis de instância
+        
+    }
+}
---
+++ Intervalo/User.java
@@ -0,0 +1,21 @@
+
+/**
+ * Escreva uma descrição da classe User aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class User
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private int id;
+
+    /**
+     * Construtor para objetos da classe Maquina
+     */
+    public User(int id)
+    {
+        // inicializa variáveis de instância
+        this.id = id;
+    }
+}
