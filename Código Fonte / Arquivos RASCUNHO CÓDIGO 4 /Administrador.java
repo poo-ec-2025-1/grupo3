9a17588f75497425839f2de40fea85ce4d7698ac
Eddclement <eduardo_silva23@discente.ufg.br>
2025-06-27 18:26:29 -0300

    Delete Código Fonte / Arquivos RASCUNHO CÓDIGO 4  directory

--- Código Fonte / Arquivos RASCUNHO CÓDIGO 4 /Administrador.java
+++
@@ -1,9 +0,0 @@
-public class Administrador extends Usuario {
-    public Administrador() {
-        // Construtor sem argumentos necessário para ORMLite
-    }
-
-    public Administrador(String nomeCompleto, int matricula, String senha) {
-        super(nomeCompleto, matricula, senha);
-    }
-}
