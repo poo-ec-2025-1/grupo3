5fc30e0a19636957d27bab6ad368df74bbfb5699
Daniel Vieira <danielvieiraeduc@gmail.com>
2025-06-30 18:32:51 -0300

    Delete PROJETO DEFINITIVO directory

--- PROJETO DEFINITIVO/lavanderia/Model/Autenticavel.java
+++
@@ -1,5 +0,0 @@
-package lavanderia.Model;
-
-public interface Autenticavel {
-    public boolean login(int matricula, String senha);
-}
--- PROJETO DEFINITIVO/lavanderia/Model/Secadora.java
+++
@@ -1,8 +0,0 @@
-package lavanderia.Model;
-
-public class Secadora extends Aparelho {
-    public Secadora(String modelo, String descricao, int capacidadeKg, boolean agendavel, double custo) {
-        super(modelo, descricao, capacidadeKg, agendavel, custo);
-
-    }
-}
