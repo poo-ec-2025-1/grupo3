86ffdf023407a53076754aebf5bb9b1537a1661a
Daniel Vieira <danielvieiraeduc@gmail.com>
2025-06-30 18:19:52 -0300

    Delete PROJETO DEFINITIVO directory

--- PROJETO DEFINITIVO/lavanderia/Model/MaquinaDeLavar.java
+++
@@ -1,8 +0,0 @@
-package lavanderia.Model;
-
-public class MaquinaDeLavar extends Aparelho {
-    public MaquinaDeLavar(String modelo, String descricao, int capacidadeKg, boolean agendavel, double custo) {
-        super(modelo, descricao, capacidadeKg, agendavel, custo);
-
-    }
-}
